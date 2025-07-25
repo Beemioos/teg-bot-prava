2c31011a23707ae90cde18778b6133512a7679a3
beemioos <beemioos@MacBook-Air-beemioos.local>
2025-07-25 13:15:10 -0700

    Fix MIME type and Netlify cоооonfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
-import { useEffect } from 'react'
-
-const tg = window.Telegram.WebApp
+import React from 'react'
 
 const App = () => {
-	useEffect(() => {
-		tg.ready()
-	}, [])
-
-	const onClose = () => {
-		tg.close()
-	}
-
-	const sendMessage = async () => {
-		try {
-			const response = await fetch(
-				'https://cf2164a07496.ngrok-free.app/send-message',
-				{
-					method: 'POST',
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify({
-						chatId: tg.initDataUnsafe?.user?.id || 'test',
-						message: 'Hello from Mini App!',
-					}),
-				}
-			)
-			const result = await response.json()
-			console.log(result)
-		} catch (error) {
-			console.error('Error:', error)
-		}
-	}
-
-	return (
-		<div>
-			App
-			<button onClick={onClose}>Close</button>
-			<button onClick={sendMessage}>Send</button>
-		</div>
-	)
+  return (
+    <div>App</div>
+  )
 }
 
 export default App
