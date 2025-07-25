74f9183a93f48a0329a3d41dc195cf71ef14961a
beemioos <beemioos@MacBook-Air-beemioos.local>
2025-07-25 12:26:54 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+const tg = window.Telegram.WebApp
+const App = () => {
+	useEffect(() => {
+		tg.ready()
+	}, [])
+	const onClose = () => {
+		tg.close()
+	}
+	return <div>App
+
+    <button onClick={onClose}>clase</button>
+  </div>
+}
+
+export default App
